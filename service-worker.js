64446921237cd18cfb3982bd05e36f59c1664972
sreeesreejith-jpg <sreee.sreejith@gmail.com>
2025-12-31 14:18:07 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,32 @@
-const CACHE_NAME = 'sree-apps-hub-v1';
+const CACHE_NAME = 'sree-apps-hub-v2';
 const ASSETS = [
     './',
     './index.html',
     './style.css',
     './manifest.json',
-    './icon-1024.jpg',
     './icon-192.png',
-    './icon-512.png'
+    './icon-512.png',
+    './icon-maskable.png'
 ];
 
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
